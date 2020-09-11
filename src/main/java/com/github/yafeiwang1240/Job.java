94947fd007bfbbc5a5955a9c84d2587aca7a70cb
wangyafei <364968487@qq.com>
2020-09-11 17:17:12 +0800

    sum reduce

--- src/main/java/com/github/yafeiwang1240/Job.java
+++ src/main/java/com/github/yafeiwang1240/Job.java
@@ -1,11 +1,8 @@
 package com.github.yafeiwang1240;
 
-import com.github.yafeiwang1240.filter.NameFilter;
 import com.github.yafeiwang1240.sink.PrintlnSink;
 import com.github.yafeiwang1240.source.SourceFactory;
-import com.github.yafeiwang1240.transformation.FlatToString;
 import com.github.yafeiwang1240.transformation.StringToTuple2;
-import com.github.yafeiwang1240.transformation.SumReduce;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -14,12 +11,11 @@ public class Job {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(60000);
         DataStreamSource<String> source = SourceFactory.newSelfStream(env);
-        DataStreamSource<String> source1 = SourceFactory.newSocketStream(env);
-        source.connect(source1).flatMap(new FlatToString<>()).filter(new NameFilter())
-                .map(new StringToTuple2())
+        source.map(new StringToTuple2())
                 .timeWindowAll(Time.seconds(10))
-                .reduce(new SumReduce())
+                .sum(1)
                 .addSink(new PrintlnSink<>());
         env.execute("test");
     }
