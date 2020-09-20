b7cefe3268afa296845574c224c4aae36d1157f8
wangyafei <364968487@qq.com>
2020-09-20 10:38:06 +0800

    checkpoint

--- src/main/java/com/github/yafeiwang1240/Job.java
+++ src/main/java/com/github/yafeiwang1240/Job.java
@@ -1,8 +1,11 @@
 package com.github.yafeiwang1240;
 
+import com.github.yafeiwang1240.filter.NameFilter;
 import com.github.yafeiwang1240.sink.PrintlnSink;
 import com.github.yafeiwang1240.source.SourceFactory;
+import com.github.yafeiwang1240.transformation.FlatToString;
 import com.github.yafeiwang1240.transformation.StringToTuple2;
+import com.github.yafeiwang1240.transformation.SumReduce;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -12,10 +15,14 @@ public class Job {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.enableCheckpointing(60000);
+        env.getCheckpointConfig().setCheckpointTimeout(30000);
         DataStreamSource<String> source = SourceFactory.newSelfStream(env);
-        source.map(new StringToTuple2())
+        DataStreamSource<String> source1 = SourceFactory.newSocketStream(env);
+        source.connect(source1).flatMap(new FlatToString<>())
+                .filter(new NameFilter())
+                .map(new StringToTuple2())
                 .timeWindowAll(Time.seconds(10))
-                .sum(1)
+                .reduce(new SumReduce())
                 .addSink(new PrintlnSink<>());
         env.execute("test");
     }
